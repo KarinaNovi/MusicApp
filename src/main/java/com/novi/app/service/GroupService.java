392cd8998ef626bc1e4fbfc6d947939abdc34e55
KarinaNovi <karinanov97@gmail.com>
2023-11-25 23:42:40 +0300

    Added basic methods for group service

--- src/main/java/com/novi/app/service/GroupService.java
+++ src/main/java/com/novi/app/service/GroupService.java
@@ -1,16 +1,26 @@
 package com.novi.app.service;
 
 import com.novi.app.model.Group;
+import com.novi.app.model.MusicInstrument;
+import com.novi.app.model.MusicStyle;
+import com.novi.app.model.User;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface GroupService {
-    // all groups with current style
     // single group operation
+    // get info
+    Optional<Group> findGroupById(Long groupId);
+    Set<User> getGroupMembers(Long groupId);
+    Set<MusicStyle> getGroupMusicStyles(Long groupId);
+    Set<MusicInstrument> getGroupMusicInstruments(Long groupId);
+    Long getGroupLeaderId(Long groupId);
+
+    // update info
     void saveGroup(Group group);
-    Optional<Group> findGroupById(Long id);
-    void deleteGroup(Long id);
+    void deleteGroup(Long groupId);
     void updateGroup(Long groupId, Group group);
 
     // many groups operation
@@ -18,5 +28,4 @@ public interface GroupService {
     List<Group> findNewlyCreatedGroups();
     List<Group> getGroupsWithOnDemandMembers(int quantityOnDemandMembers);
     List<Group> getGroupsWithCurrentMusicStyle(int styleId);
-
 }
--- src/main/java/com/novi/app/service/impl/GroupServiceImpl.java
+++ src/main/java/com/novi/app/service/impl/GroupServiceImpl.java
@@ -1,15 +1,16 @@
 package com.novi.app.service.impl;
 
 import com.novi.app.model.Group;
+import com.novi.app.model.MusicInstrument;
+import com.novi.app.model.MusicStyle;
+import com.novi.app.model.User;
 import com.novi.app.model.repository.GroupRepository;
 import com.novi.app.service.GroupService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Optional;
+import java.lang.constant.Constable;
+import java.util.*;
 
 @Service
 @Transactional
@@ -35,6 +36,58 @@ public class GroupServiceImpl implements GroupService {
     @Override
     public Optional<Group> findGroupById(Long id) {
         return groupRepository.findById(id);
+    }
+
+    @Override
+    public Set<User> getGroupMembers(Long groupId) {
+        Optional<Group> optionalGroup = findGroupById(groupId);
+        Set<User> groupMembers = new HashSet<>();
+        if (optionalGroup.isPresent()) {
+            Group group = optionalGroup.get();
+            groupMembers.addAll(group.getUsers());
+        } else {
+            System.out.println("WARN: No existing group with such id");
+        }
+        return groupMembers;
+    }
+
+    @Override
+    public Set<MusicStyle> getGroupMusicStyles(Long groupId) {
+        Optional<Group> optionalGroup = findGroupById(groupId);
+        Set<MusicStyle> musicStyles = new HashSet<>();
+        if (optionalGroup.isPresent()) {
+            Group group = optionalGroup.get();
+            musicStyles.addAll(group.getMusicStyles());
+        } else {
+            System.out.println("WARN: No existing group with such id");
+        }
+        return musicStyles;
+    }
+
+    @Override
+    public Set<MusicInstrument> getGroupMusicInstruments(Long groupId) {
+        Optional<Group> optionalGroup = findGroupById(groupId);
+        Set<MusicInstrument> musicInstruments = new HashSet<>();
+        if (optionalGroup.isPresent()) {
+            Group group = optionalGroup.get();
+            musicInstruments.addAll(group.getMusicInstruments());
+        } else {
+            System.out.println("WARN: No existing group with such id");
+        }
+        return musicInstruments;
+    }
+
+    // TODO: maybe return user from userService
+    @Override
+    public Long getGroupLeaderId(Long groupId) {
+        Optional<Group> optionalGroup = findGroupById(groupId);
+        if (optionalGroup.isPresent()) {
+            Group group = optionalGroup.get();
+            return group.getLeaderId();
+        } else {
+            System.out.println("WARN: No existing user with such id");
+            return null;
+        }
     }
 
     @Override
